0106d93fb8b9302265b66de1d72b68dee292fb03
Abby Christens <achristens@gmail.com>
2017-10-23 17:48:15 -0400

    Starting reservation methods

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,16 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
 
-  
+  # As a customer, if I try to make a reservation at a time when a restaurant is already
+  # at max capacity, the reservation will not be made and I will be shown a message indicating why.
+
+  def self.book_seats
+  # when reservation is made, seats -= party_size
+    
+
+  end
+
+  def max_capacity
+
+  end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,16 @@ class Restaurant < ApplicationRecord
   has_many :reservations
   has_many :users, through: :reservations
 
+
+  def method
+    if reservation.date.party_size <= seats
+      # confirm booking
+    else
+      flash[:notice] = "We're fully booked on that night"
+    end
+
+  end
+
+
+
 end
