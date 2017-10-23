a47c837b0e108b6d877ce1f98295fd1b4b62f060
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-23 16:11:47 -0400

    Set Restaurant controller info for Create and New methods

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,25 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new
+
+    @restaurant.name = params[:restaurant][:name]
+    @restaurant.address = params[:restaurant][:address]
+    @restaurant.open_time = params[:restaurant][:open_time]
+    @restaurant.close_time = params[:restaurant][:close_time]
+    @restaurant.user_id = current_user
+
+    if @restaurant.save
+      render root_url
+    else
+      render :new
+    end
+  end
+
 end
