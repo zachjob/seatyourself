bd3b70844cbac4df82bbaa9a7ecc0683d3ab3197
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-23 15:43:30 -0400

    Create sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,24 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.phone = params[:user][:phone]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save
+      redirect_to root_url
+    else
+      render :new
+    end
 
   end
+
 end
