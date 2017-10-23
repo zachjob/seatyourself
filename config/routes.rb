2001a3592df38804922b0e979f6a19c6e659f327
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-23 15:37:52 -0400

    Create routes for sessions controller and add link to sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'restaurants#index'
-
+  resource :session, only: [:new, :create, :destroy]
   resource :users
   resources :restaurants
 
