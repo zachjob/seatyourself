335eca2d9604bb6d864be1e3554c22968050ba41
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-23 16:00:07 -0400

    Get sign in working correctly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email:params[:email])
+    @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to root_url
@@ -14,6 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
+    session[:user_id] = nil
+    redirect_to root_url
   end
 end
