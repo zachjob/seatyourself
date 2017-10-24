473a23f1875e04b003f0ac87581cc47576890227
Zach Job <zachery.job@gmail.com>
2017-10-24 10:29:04 -0400

    Add pry-rails to gemfile, add validation to check whether or not the reservation is in the past

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,12 +2,20 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
 
+  validate :not_past_date
+
+  def not_past_date
+    if self.date < Date.today
+      errors.add(:date, 'not in past')
+    end
+  end
+
   # As a customer, if I try to make a reservation at a time when a restaurant is already
   # at max capacity, the reservation will not be made and I will be shown a message indicating why.
 
   def self.book_seats
   # when reservation is made, seats -= party_size
-    
+
 
   end
 
