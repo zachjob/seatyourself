de388a3ddf1939b55b8145cd0abf6bb8d4a32340
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-24 15:09:02 -0400

    Add validation for user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :restaurants
   has_many :reservation_restaurants, through: :reservations, source: :restaurant
 
+  validates :name, :email, :password, :password_confirmation, presence: true
+
 
   has_secure_password
 end
