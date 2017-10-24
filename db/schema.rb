dacfaa13c6887c7afd766602b3e510ea8e139b13
Zach Job <zachery.job@gmail.com>
2017-10-24 14:53:34 -0400

    Add loyalty-points column to users-table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024181456) do
+ActiveRecord::Schema.define(version: 20171024185143) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "user_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20171024181456) do
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "loyalty_points"
   end
 
 end
