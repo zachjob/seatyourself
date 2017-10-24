c01b3d2afe7068a80c619019c5efa71b8b5308a5
Abby Christens <achristens@gmail.com>
2017-10-24 14:30:53 -0400

    Readding seat validation in Reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,7 +3,8 @@ class Reservation < ApplicationRecord
   belongs_to :restaurant
 
   validate :not_past_date
-
+  validate :book_seats
+  
   def not_past_date
     if self.date < Date.today
       errors.add(:date, 'not in past')
@@ -19,10 +20,14 @@ class Reservation < ApplicationRecord
   # As a customer, if I try to make a reservation at a time when a restaurant is already
   # at max capacity, the reservation will not be made and I will be shown a message indicating why.
 
-  def self.book_seats
-  # when reservation is made, seats -= party_size
+  def book_seats
+    booked_seats = Reservation.where(time: time).where(restaurant_id: restaurant_id).sum(:party_size)
+    available_seats = Restaurant.find(restaurant_id).seats - booked_seats
 
-
+    if party_size <= available_seats
+    else
+      self.errors.add(:base, "Sorry, we are do have any tables available at that time.")
+    end
   end
 
   def max_capacity
