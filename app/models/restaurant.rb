ab61cd80cf1c6622ae418626319e99fab295d827
Abby Christens <achristens@gmail.com>
2017-10-25 16:24:45 -0400

    Convert time to 12 clock

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,5 +5,9 @@ class Restaurant < ApplicationRecord
 
   validates :name, :open_time, :close_time, :seats, :address, :price_range, :summary, :menu, :min_seating, :max_seating, presence: true
 
-
+  # def convert_time(time)
+  #   if time > 12
+  #     time.map { |hour| Time.parse("#{hour}:00").strftime("%l %P") }
+  #   end
+  # end
 end
