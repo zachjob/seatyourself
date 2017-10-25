19e697711da2da2b6c661ba39808928dce87c76d
Abby Christens <achristens@gmail.com>
2017-10-25 14:52:31 -0400

    Organize owner dashboard

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -41,11 +41,11 @@ class ReservationsController < ApplicationController
     if @reservation.present?
       @reservation.destroy
 
-      current_user.loyalty_points -= 10
-      current_user.save
+      # current_user.loyalty_points -= 10
+      # current_user.save
       flash[:notice] = "Reservation has been successfully deleted."
     end
     redirect_to root_url
   end
-  
+
 end
